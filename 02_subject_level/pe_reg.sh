62a9be62f8c099531f86724def530c3d0dca3b4a
oliviakowalczyk <72813614+oliviakowalczyk@users.noreply.github.com>
2025-08-18 09:32:24 +0100

    paths updated

--- 02_subject_level/pe_reg.sh
+++ 02_subject_level/pe_reg.sh
@@ -1,6 +1,6 @@
 # Path of the folder containing all data
-dir="/data/project/SPAIN/derivatives/squeeze"
-sct_dir="/nan/ceph/network/system/el8/sct/6.5"
+dir="/root/dir"
+sct_dir="/sct/dir/6.5"
 
 # List of subjects
 declare -a sub=("sub-SPAIN01" "sub-SPAIN02" "sub-SPAIN04" "sub-SPAIN06" "sub-SPAIN07" "sub-SPAIN08" "sub-SPAIN11" "sub-SPAIN13" "sub-SPAIN15" "sub-SPAIN16" "sub-SPAIN19" "sub-SPAIN20" "sub-SPAIN21" "sub-SPAIN24" "sub-SPAIN25" "sub-SPAIN26" "sub-SPAIN27" "sub-SPAIN28" "sub-SPAIN30" "sub-SPAIN31" "sub-SPAIN32" "sub-SPAIN36" "sub-SPAIN38" "sub-SPAIN39" "sub-SPAIN40" "sub-SPAIN41" "sub-SPAIN44" "sub-SPAIN49" "sub-SPAIN52" "sub-SPAIN55")
