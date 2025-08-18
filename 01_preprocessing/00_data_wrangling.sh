5b1c46f996e1345683fd7d865840ace2f75753fa
oliviakowalczyk <72813614+oliviakowalczyk@users.noreply.github.com>
2025-08-18 08:55:01 +0100

    paths updated

--- 01_preprocessing/00_data_wrangling.sh
+++ 01_preprocessing/00_data_wrangling.sh
@@ -1,5 +1,5 @@
 # Path of the folder containing all data
-dir="root/dir"
+dir="/root/dir"
 
 # List of cervical spine images
 declare -a sub=("sub-SPAIN01" "sub-SPAIN02" "sub-SPAIN04" "sub-SPAIN06" "sub-SPAIN07" "sub-SPAIN08" "sub-SPAIN11" "sub-SPAIN13" "sub-SPAIN15" "sub-SPAIN16" "sub-SPAIN19" "sub-SPAIN20" "sub-SPAIN21" "sub-SPAIN24" "sub-SPAIN25" "sub-SPAIN26" "sub-SPAIN27" "sub-SPAIN28" "sub-SPAIN30" "sub-SPAIN31" "sub-SPAIN32" "sub-SPAIN36" "sub-SPAIN38" "sub-SPAIN39" "sub-SPAIN40" "sub-SPAIN41" "sub-SPAIN44" "sub-SPAIN49" "sub-SPAIN52" "sub-SPAIN55")
