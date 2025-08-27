614bb8a947319b64b1a2913abd807afb57907737
oliviakowalczyk <72813614+oliviakowalczyk@users.noreply.github.com>
2025-08-27 12:14:42 +0100

    update to mean signal extraction

--- 04_reliability/icc/02_extract_mean_roi.sh
+++ 04_reliability/icc/02_extract_mean_roi.sh
@@ -1,55 +1,112 @@
 # Path of the folder containing all data
 dir="/root/dir"
 
-# Threshold t-stat image with FWE-corr TFCE map
-fslmaths ${dir}/derivatives/group_level/randomise_first_trial_censored_run-1-avg/cope1/one_sampt_tfce_corrp_tstat1.nii.gz \
--thr 0.95 -bin -mul ${dir}/derivatives/group_level/randomise_first_trial_censored_run-1-avg/cope1/one_sampt_tstat1.nii.gz \
-${dir}/derivatives/group_level/randomise_first_trial_censored_run-1-avg/cope1/one_sampt_tstat1_thr_0.05.nii.gz
+# Define conditions and mask types
+declare -A analyses=(
+    ["standard"]="randomise_first_trial_censored"
+    ["paramod"]="randomise_first_trial_censored_paramod"
+)
+declare -A avgs=(
+    ["run-1-avg"]="run-13-avg"
+    ["run-2-avg"]="run-24-avg"
+    ["ses-A-avg"]="run-12-avg"
+    ["ses-B-avg"]="run-34-avg"
+)
 
-# Binarise the thresholded t-stat image
-fslmaths ${dir}/derivatives/group_level/randomise_first_trial_censored_run-1-avg/cope1/one_sampt_tstat1_thr_0.05.nii.gz -bin \
-${dir}/derivatives/group_level/randomise_first_trial_censored_run-1-avg/cope1/one_sampt_tstat1_thr_0.05_bin.nii.gz
+# Threshold and binarise t-stat images for FWE coorrection
+for paramod in "standard" "paramod"; do
+	analysis=${analyses[$paramod]}
+	for key in "run-1-avg" "ses-A-avg"; do
+		avg=${avgs[$key]}
+		echo "Processing ${analysis}_${avg}..."
 
-fslmaths ${dir}/derivatives/group_level/randomise_first_trial_censored_ses-A-avg/cope1/one_sampt_tfce_corrp_tstat1.nii.gz \
--thr 0.95 -bin -mul ${dir}/derivatives/group_level/randomise_first_trial_censored_ses-A-avg/cope1/one_sampt_tstat1.nii.gz \
-${dir}/derivatives/group_level/randomise_first_trial_censored_ses-A-avg/cope1/one_sampt_tstat1_thr_0.05.nii.gz
+		# Threshold
+		fslmaths ${dir}/derivatives/group_level/${analysis}_${avg}/cope1/one_sampt_tfce_corrp_tstat1.nii.gz \
+			-thr 0.95 -bin -mul ${dir}/derivatives/group_level/${analysis}_${avg}/cope1/one_sampt_tstat1.nii.gz \
+			${dir}/derivatives/group_level/${analysis}_${avg}/cope1/one_sampt_tstat1_thr_0.05.nii.gz
 
-fslmaths ${dir}/derivatives/group_level/randomise_first_trial_censored_ses-A-avg/cope1/one_sampt_tstat1_thr_0.05.nii.gz -bin \
-${dir}/derivatives/group_level/randomise_first_trial_censored_ses-A-avg/cope1/one_sampt_tstat1_thr_0.05_bin.nii.gz
+		# Binarise
+		fslmaths ${dir}/derivatives/group_level/${analysis}_${avg}/cope1/one_sampt_tstat1_thr_0.05.nii.gz -bin \
+			${dir}/derivatives/group_level/${analysis}_${avg}/cope1/one_sampt_tstat1_thr_0.05_bin.nii.gz
+	done
+done
 
-# Extract mean signal from the mask
-fslmeants -i ${dir}/derivatives/group_level/data_stacks/cope1_run-1-avg_first_trial_censored_stack.nii.gz \
--o ${dir}/derivatives/reliability/icc/mean_z/cope1_run-1_network_mean.txt \
--m ${dir}/derivatives/group_level/randomise_first_trial_censored_run-1-avg/cope1/one_sampt_tstat1_thr_0.05_bin.nii.gz
+# Threshold and binarise t-stat images for uncorrected results
+for paramod in "standard" "paramod"; do
+	analysis=${analyses[$paramod]}
+	for key in "run-1-avg" "ses-A-avg"; do
+		avg=${avgs[$key]}
+		echo "Processing ${analysis}_${avg}..."
 
-fslmeants -i ${dir}/derivatives/group_level/data_stacks/cope1_run-2-avg_first_trial_censored_stack.nii.gz \
--o ${dir}/derivatives/reliability/icc/mean_z/cope1_run-2_network_mean.txt \
--m ${dir}/derivatives/group_level/randomise_first_trial_censored_run-1-avg/cope1/one_sampt_tstat1_thr_0.05_bin.nii.gz
+		fslmaths ${dir}/derivatives/group_level/${analysis}_${avg}/cope1/one_sampt_tstat1.nii.gz -thr 3.5 -bin \
+			${dir}/derivatives/group_level/${analysis}_${avg}/cope1/one_sampt_tstat1_thr_uncorr_bin.nii.gz
+	done
+done
 
-fslmeants -i ${dir}/derivatives/group_level/data_stacks/cope1_ses-A-avg_first_trial_censored_stack.nii.gz \
--o ${dir}/derivatives/reliability/icc/mean_z/cope1_ses-A_network_mean.txt \
--m ${dir}/derivatives/group_level/randomise_first_trial_censored_ses-A-avg/cope1/one_sampt_tstat1_thr_0.05_bin.nii.gz
+# Extract mean signal from the FWE-corrected mask
+for paramod in "standard" "paramod"; do
+	analysis=${analyses[$paramod]}
+	for key in "${!avgs[@]}"; do
+		if [[ $key == "run-1-avg" || $key == "run-2-avg" ]]; then
+			mask="run-13-avg"
+		else
+			mask="run-12-avg"
+		fi
+		avg=${avgs[$key]}
+		if [[ $paramod == "standard" ]]; then
+			in_stack="cope1_${avg}_first_trial_censored_stack.nii.gz"
+			out_name="cope1_${key}_network_mean.txt"
+		else
+			in_stack="cope1_${avg}_first_trial_censored_paramod_stack.nii.gz"
+			out_name="cope1_${key}_paramod_network_mean.txt"
+		fi
+		echo "Extracting mean for ${out_name} from ${in_stack} using mask ${mask}..."
+        fslmeants -i ${dir}/derivatives/group_level/data_stacks/${in_stack} \
+        	-o ${dir}/derivatives/reliability/icc/mean_t/${out_name} \
+        	-m ${dir}/derivatives/group_level/${analysis}_${mask}/cope1/one_sampt_tstat1_thr_0.05_bin.nii.gz
+    done
+done
 
-fslmeants -i ${dir}/derivatives/group_level/data_stacks/cope1_ses-B-avg_first_trial_censored_stack.nii.gz \
--o ${dir}/derivatives/reliability/icc/mean_z/cope1_ses-B_network_mean.txt \
--m ${dir}/derivatives/group_level/randomise_first_trial_censored_ses-A-avg/cope1/one_sampt_tstat1_thr_0.05_bin.nii.gz
+# Extract mean signal from the uncorrected mask
+for paramod in "standard" "paramod"; do
+	analysis=${analyses[$paramod]}
+	for key in "${!avgs[@]}"; do
+		if [[ $key == "run-1-avg" || $key == "run-2-avg" ]]; then
+			mask="run-13-avg"
+		else
+			mask="run-12-avg"
+		fi
+		avg=${avgs[$key]}
+		if [[ $paramod == "standard" ]]; then
+			in_stack="cope1_${avg}_first_trial_censored_stack.nii.gz"
+			out_name="cope1_${key}_network_uncorr_mean.txt"
+		else
+			in_stack="cope1_${avg}_first_trial_censored_paramod_stack.nii.gz"
+			out_name="cope1_${key}_paramod_network_uncorr_mean.txt"
+		fi
+		echo "Extracting mean for ${out_name} from ${in_stack} using mask ${mask}..."
+        fslmeants -i ${dir}/derivatives/group_level/data_stacks/${in_stack} \
+        	-o ${dir}/derivatives/reliability/icc/mean_t/${out_name} \
+        	-m ${dir}/derivatives/group_level/${analysis}_${mask}/cope1/one_sampt_tstat1_thr_uncorr_bin.nii.gz
+    done
+done
 
 # Extract mean signal from each ROI
-for i in "spinal_level_c5" "spinal_level_c6" "spinal_level_c7" "spinal_level_c8" "spinal_level_t1" "cord"; do
-
-	fslmeants -i ${dir}/derivatives/group_level/data_stacks/cope1_run-1-avg_first_trial_censored_stack.nii.gz \
-	-o ${dir}/derivatives/reliability/icc/mean_z/cope1_run-1_${i}_mean.txt \
-	-m ${dir}/masks/PAM50_${i}.nii.gz
-	
-	fslmeants -i ${dir}/derivatives/group_level/data_stacks/cope1_run-2-avg_first_trial_censored_stack.nii.gz \
-	-o ${dir}/derivatives/reliability/icc/mean_z/cope1_run-2_${i}_mean.txt \
-	-m ${dir}/masks/PAM50_${i}.nii.gz
-	
-	fslmeants -i ${dir}/derivatives/group_level/data_stacks/cope1_ses-A-avg_first_trial_censored_stack.nii.gz \
-	-o ${dir}/derivatives/reliability/icc/mean_z/cope1_ses-A_${i}_mean.txt \
-	-m ${dir}/masks/PAM50_${i}.nii.gz
-	
-	fslmeants -i ${dir}/derivatives/group_level/data_stacks/cope1_ses-B-avg_first_trial_censored_stack.nii.gz \
-	-o ${dir}/derivatives/reliability/icc/mean_z/cope1_ses-B_${i}_mean.txt \
-	-m ${dir}/masks/PAM50_${i}.nii.gz
+rois=("spinal_level_c5" "spinal_level_c6" "spinal_level_c7" "spinal_level_c8" "spinal_level_t1" "cord")
+for i in "${rois[@]}"; do
+    for paramod in "standard" "paramod"; do
+        for key in "${!avgs[@]}"; do
+            if [[ $paramod == "standard" ]]; then
+                in_stack="cope1_${avgs[$key]}_first_trial_censored_stack.nii.gz"
+                out_name="cope1_${key}_${i}_mean.txt"
+            else
+                in_stack="cope1_${avgs[$key]}_first_trial_censored_${paramod}_stack.nii.gz"
+                out_name="cope1_${key}_${paramod}_${i}_mean.txt"
+            fi
+			echo "Extracting mean for ${out_name} from ${in_stack}..."
+            fslmeants -i ${dir}/derivatives/group_level/data_stacks/${in_stack} \
+            	-o ${dir}/derivatives/reliability/icc/mean_t/${out_name} \
+            	-m ${dir}/masks/PAM50_${i}.nii.gz
+        done
+    done
 done
---
+++ 04_reliability/icc/03_extract_mean_roi_txt2csv.sh
@@ -0,0 +1,92 @@
+# Paths
+dir="/root/dir"
+csv_file="${dir}/derivatives/reliability/icc/mean_t/mean_master.csv"
+txt_dir="${dir}/derivatives/reliability/icc/mean_t"
+
+# Define order for sessions and ROIs
+sessions=("run-1-avg" "run-2-avg" "ses-A-avg" "ses-B-avg")
+roi_order=("cord" "network" "network_uncorr" "spinal_level_c5" "spinal_level_c6" "spinal_level_c7" "spinal_level_c8" "spinal_level_t1")
+
+# Collect all txt files and their column names
+declare -a file_list=()
+declare -a header_list=()
+
+# Build file list and headers in the specified order: condition-roi-session
+for condition in "standard" "paramod"; do
+    for roi in "${roi_order[@]}"; do
+        for session in "${sessions[@]}"; do
+            if [[ $roi == "network" ]]; then
+                # Network files have different naming convention
+                if [[ $condition == "standard" ]]; then
+                    txt_file="${txt_dir}/cope1_${session}_network_mean.txt"
+                    col_name="${condition}_${roi}_${session}"
+                else
+                    txt_file="${txt_dir}/cope1_${session}_paramod_network_mean.txt"
+                    col_name="${condition}_${roi}_${session}"
+                fi
+            # Network uncorrected files have different naming convention
+            elif [[ $roi == "network_uncorr" ]]; then
+                if [[ $condition == "standard" ]]; then
+                    txt_file="${txt_dir}/cope1_${session}_network_uncorr_mean.txt"
+                    col_name="${condition}_${roi}_${session}"
+                else
+                    txt_file="${txt_dir}/cope1_${session}_paramod_network_uncorr_mean.txt"
+                    col_name="${condition}_${roi}_${session}"
+                fi
+            else
+                # ROI files
+                if [[ $condition == "standard" ]]; then
+                    txt_file="${txt_dir}/cope1_${session}_${roi}_mean.txt"
+                    col_name="${condition}_${roi}_${session}"
+                else
+                    txt_file="${txt_dir}/cope1_${session}_paramod_${roi}_mean.txt"
+                    col_name="${condition}_${roi}_${session}"
+                fi
+            fi
+            
+            # Add to arrays
+            file_list+=("$txt_file")
+            header_list+=("$col_name")
+        done
+    done
+done
+
+# Find the maximum number of lines across all files
+max_lines=0
+for file in "${file_list[@]}"; do
+    if [[ -f "$file" ]]; then
+        lines=$(wc -l < "$file")
+        if [[ $lines -gt $max_lines ]]; then
+            max_lines=$lines
+        fi
+    fi
+done
+
+# Create header
+header="Row"
+for col in "${header_list[@]}"; do
+    header="${header},${col}"
+done
+echo "${header}" > ${csv_file}
+
+# Create data rows
+for ((row=1; row<=max_lines; row++)); do
+    data_row="$row"
+    for file in "${file_list[@]}"; do
+        if [[ -f "$file" ]]; then
+            value=$(sed -n "${row}p" "$file" | tr -d '\n')  # Get specific line
+            if [[ -z "$value" ]]; then
+                value="NA"  # If line doesn't exist
+            fi
+        else
+            value="NA"
+        fi
+        data_row="${data_row},${value}"
+    done
+    echo "${data_row}" >> ${csv_file}
+done
+
+echo "CSV file created: ${csv_file}"
+echo "Total columns: $((${#header_list[@]} + 1))"
+echo "Total rows: $((max_lines + 1))"
+echo "Files processed: ${#file_list[@]}"
