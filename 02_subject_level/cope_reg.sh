acd8e0f3d3f007601b30a3fe9baa4fe1c6b67ccc
oliviakowalczyk <72813614+oliviakowalczyk@users.noreply.github.com>
2026-02-23 20:16:06 +0000

    rootlet-based reg update

--- 02_subject_level/cope_reg.sh
+++ 02_subject_level/cope_reg.sh
@@ -1,6 +1,6 @@
 # Path of the folder containing all data
 dir="/root/dir"
-sct_dir="/sct/dir/6.5"
+sct_dir="/sct/dir/7.0"
 
 # List of subjects
 declare -a sub=("sub-SPAIN01" "sub-SPAIN02" "sub-SPAIN04" "sub-SPAIN06" "sub-SPAIN07" "sub-SPAIN08" "sub-SPAIN11" "sub-SPAIN13" "sub-SPAIN15" "sub-SPAIN16" "sub-SPAIN19" "sub-SPAIN20" "sub-SPAIN21" "sub-SPAIN24" "sub-SPAIN25" "sub-SPAIN26" "sub-SPAIN27" "sub-SPAIN28" "sub-SPAIN30" "sub-SPAIN31" "sub-SPAIN32" "sub-SPAIN36" "sub-SPAIN38" "sub-SPAIN39" "sub-SPAIN40" "sub-SPAIN41" "sub-SPAIN44" "sub-SPAIN49" "sub-SPAIN52" "sub-SPAIN55")
@@ -15,7 +15,23 @@ for i in "${sub[@]}"; do
             echo "Processing " ${i} ${j} ${l}
             for m in "cope1" "cope2"; do
 
-			    sct_apply_transfo -i ${m}.nii.gz -d ${sct_dir}/data/PAM50/template/PAM50_t2s.nii.gz -w ${dir}/derivatives/preprocessing/${i}/${j}/${l}/warp_fmri2template.nii.gz -x linear
+			    sct_apply_transfo -i ${m}.nii.gz -d ${sct_dir}/data/PAM50/template/PAM50_t2s.nii.gz -w ${dir}/derivatives/preprocessing/${i}/${j}/${l}/reg_rootlets/warp_chop_MCnofilt_tmean2PAM50_t2s.nii.gz -x linear
+			done
+        done
+    done
+done
+
+# Loop over all subjects for parametric modulation analysis
+for i in "${sub[@]}"; do
+	for j in "ses-A" "ses-B"; do
+        for l in "run-1" "run-2"; do
+			
+            cd $dir/derivatives/subject_level/${i}_${j}_${l}_first_trial_censored_paramod.feat/stats
+
+            echo "Processing " ${i} ${j} ${l}
+            for m in "cope1" "cope2" "cope3"; do
+
+			    sct_apply_transfo -i ${m}.nii.gz -d ${sct_dir}/data/PAM50/template/PAM50_t2s.nii.gz -w ${dir}/derivatives/preprocessing/${i}/${j}/${l}/reg_rootlets/warp_chop_MCnofilt_tmean2PAM50_t2s.nii.gz -x linear
 			done
         done
     done
